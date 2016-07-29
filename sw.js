e417b9a99a91ce8bacd685c1d565db493b53b3ec
Tariq Khan <tariq@inspirythemes.com>
2016-07-29 16:25:56 +0500

    MF

--- sw.js
+++ sw.js
@@ -2,16 +2,15 @@ var CACHE_NAME = 'Tariq-Khan-Site-Cache-v1';
 var urlsToCache = [
     '/',
     '/css/main.css',
-    '/js/jquery.js'
+    '/js/jquery.min.js'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     // Perform install steps
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
     );
 });
