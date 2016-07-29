241b7e8bdd11179e3688d6a414db09b99317265e
Tariq Khan <tariq@inspirythemes.com>
2016-07-29 12:07:21 +0500

    Updated files

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-if (navigator) {
+//if (navigator) {
+//
+//    // Adding New Custom Object to Navigator
+//    navigator.tarqkhan = {
+//        name: function () {
+//            console.log('My name is Tariq Khan');
+//        },
+//        doing: function () {
+//            console.log('I am working :)');
+//        }
+//    };
+//
+//    console.log( navigator.tarqkhan );
+//}
+//
 
-    // Adding New Custom Object to Navigator
-    navigator.tarqkhan = {
-        name: function () {
-            console.log('My name is Tariq Khan');
-        },
-        doing: function () {
-            console.log('I am working :)');
-        }
-    };
-
-    console.log( navigator.tarqkhan );
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+    }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
 }
