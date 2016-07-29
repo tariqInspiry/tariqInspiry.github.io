168b23f9f06617cbc1c33e2ad058ada38195f9a9
Tariq Khan <tariq@inspirythemes.com>
2016-07-29 16:50:12 +0500

    MF

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-//
 if ('serviceWorker' in navigator) {
     //navigator.serviceWorker.register('/sw.js').then(function(registration) {
     //    // Registration was successful
@@ -10,16 +8,16 @@ if ('serviceWorker' in navigator) {
     //});
 
 
-    // scope defaults to the path the script sits in
+// scope defaults to the path the script sits in
 // "/" in this example
-navigator.serviceWorker.register("/sw.js").then(
-    function(registration) {
-        console.log("success!");
-        if (registration.installing) {
-            registration.installing.postMessage("Howdy from your installing page.");
-        }
-    },
-    function(why) {
-        console.error("Installing the worker failed!:", why);
-    });
+    navigator.serviceWorker.register("/sw.js").then(
+        function (registration) {
+            console.log("success!");
+            if (registration.installing) {
+                registration.installing.postMessage("Howdy from your installing page.");
+            }
+        },
+        function (why) {
+            console.error("Installing the worker failed!:", why);
+        });
 }
