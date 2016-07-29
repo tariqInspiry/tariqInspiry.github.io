33c3ad378802fb9252979cca5fd4cfb72ec1b09d
Tariq Khan <tariq@inspirythemes.com>
2016-07-29 12:23:26 +0500

    MF

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-//if (navigator) {
-//
-//    // Adding New Custom Object to Navigator
-//    navigator.tarqkhan = {
-//        name: function () {
-//            console.log('My name is Tariq Khan');
-//        },
-//        doing: function () {
-//            console.log('I am working :)');
-//        }
-//    };
-//
-//    console.log( navigator.tarqkhan );
-//}
-//
 
+//
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ',    registration.scope);
-    }).catch(function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-    });
+    //navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    //    // Registration was successful
+    //    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+    //}).catch(function(err) {
+    //    // registration failed :(
+    //    console.log('ServiceWorker registration failed: ', err);
+    //});
+
+
+    // scope defaults to the path the script sits in
+// "/" in this example
+    navigator.serviceWorker.register("/sw.js").then(
+        function(registration) {
+            console.log("success!");
+            if (registration.installing) {
+                registration.installing.postMessage("Howdy from your installing page.");
+            }
+        },
+        function(why) {
+            console.error("Installing the worker failed!:", why);
+        });
 }
