9bebeae112a9b12a2afca3650e8f347bcd36f469
Tariq Khan <tariq@inspirythemes.com>
2016-07-29 16:18:51 +0500

    MF

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ if ('serviceWorker' in navigator) {
 
     // scope defaults to the path the script sits in
 // "/" in this example
-    navigator.serviceWorker.register("/sw.js").then(
-        function(registration) {
-            console.log("success!");
-            if (registration.installing) {
-                registration.installing.postMessage("Howdy from your installing page.");
-            }
-        },
-        function(why) {
-            console.error("Installing the worker failed!:", why);
-        });
+navigator.serviceWorker.register("/sw.js").then(
+    function(registration) {
+        console.log("success!");
+        if (registration.installing) {
+            registration.installing.postMessage("Howdy from your installing page.");
+        }
+    },
+    function(why) {
+        console.error("Installing the worker failed!:", why);
+    });
 }
---
+++ sw.js
@@ -0,0 +1,17 @@
+var CACHE_NAME = 'Tariq-Khan-Site-Cache-v1';
+var urlsToCache = [
+    '/',
+    '/css/main.css',
+    '/js/jquery.js'
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
